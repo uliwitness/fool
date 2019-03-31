42128736f4ea6277b48a78ed7075cbb18eee5607
Uli Kusterer <ulivc@zathras.de>
2019-03-31 19:41:01 +0200

    Initial Commit

---
+++ fool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fool
+//
+//  Created by Uli Kusterer on 31.03.19.
+//  Copyright © 2019 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
